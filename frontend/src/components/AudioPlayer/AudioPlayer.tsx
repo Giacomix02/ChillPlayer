7e843ec6fbeefa1f1efb66597cccfd02acf7e69b
Giacomix02 <paoloccigiacomo@gmail.com>
2025-06-17 00:04:25 +0200

    la barra delle canzone si muove, il titolo del file viene visualizzato

--- frontend/src/components/AudioPlayer/AudioPlayer.tsx
+++ frontend/src/components/AudioPlayer/AudioPlayer.tsx
@@ -1,20 +1,25 @@
 'use client'
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {open} from "@tauri-apps/plugin-dialog";
 import {readFile} from "@tauri-apps/plugin-fs";
 import s from "./AudioPlayer.module.css";
-import { FaPlay, FaPause  } from "react-icons/fa";
+import { FaPlay, FaPause, FaStepForward, FaStepBackward  } from "react-icons/fa";
 
 export default function AudioPlayer() {
 
     //const audioPlayer = document.getElementById('audioPlayer') as HTMLAudioElement;
     const audioPlayer = useRef<HTMLAudioElement>(null)
-    const [path, setPath] = useState('');
+    const [path, setPath] = useState<string|null>(null);
+    const [currentTime, setCurrentTime] = useState<number>(0); //expressed in seconds
+    const [duration, setDuration] = useState<number>(0); //expressed in seconds
+    const [play, setPlay] = useState<boolean>(true);
+    const[name, setName] = useState<string>("Unknown Title");
 
-    const [play, setPlay] = useState(true);
+
 
     async function loadSong() {
         console.log("Load song clicked");
+        setPlay(true)
         const file = await open({
             multiple: false,
             directory: false,
@@ -26,7 +31,14 @@ export default function AudioPlayer() {
         const data = await readFile(file);
         const blob = new Blob([new Uint8Array(data)], { type: 'audio/mpeg' });
         const fileUrl = URL.createObjectURL(blob);
+        console.log("File URL:", fileUrl);
         setPath(fileUrl);
+
+        const fileNameWithExt = file.split(/[\\/]/).pop() || "Unknown Title";
+        const fileName = fileNameWithExt.replace(/\.[^/.]+$/, "") || "Unknown Title";
+        setName(fileName);
+
+
     }
     function playSong() {
         console.log("Play song clicked");
@@ -43,17 +55,43 @@ export default function AudioPlayer() {
 
     return (
         <div className={s.back}>
+            <div className={s.left}>
+                <a>{name}</a>
+            </div>
+
+            <div className={s.center}>
+                <input className={s.timeline} type="range" name="Timeline" min="0" max={duration*10000}
+                       defaultValue={currentTime*10000}/>
+            </div>
+
+
+            <div className={s.right}>
+                <div className={s.rightContainer}>
+                    <button>
+                        <FaStepBackward className={s.nextBackButton}/>
+                    </button>
+                    <button type="button" onClick={play ? playSong : stopSong}>
+                        {play ? <FaPlay className={s.playButton}/> : <FaPause className={s.pauseButton}/>}
+                    </button>
+                    <button>
+                        <FaStepForward className={s.nextBackButton}/>
+                    </button>
+                </div>
+            </div>
+
             <button type="button" onClick={loadSong}>
                 Load Song
             </button>
-            <button type="button" onClick={play? playSong : stopSong}>
-                {play ? <FaPlay className={s.playButton} /> : <FaPause className={s.pauseButton} />}
-            </button>
-            <a>{path}</a>
             {
-                path != '' &&
-                <audio ref={audioPlayer} preload="auto" src={path}></audio>
+                path ? <audio
+                    ref={audioPlayer}
+                    preload="auto" src={path}
+                    onTimeUpdate={() => setCurrentTime(audioPlayer.current?.currentTime || 0)}
+                    onLoadedMetadata={() => setDuration(audioPlayer.current?.duration || 0)}
+                    onEnded={() => setPlay(true)}
+                ></audio> : null
             }
+
         </div>
     );
 }
